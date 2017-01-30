7e94a395e7e42f1676e701d3e0b10cf2828276e0
Mieszko Wawrzyniak <Mieszko Wawrzyniak>
2017-01-30 13:50:49 +0100

    Add project files.

---
+++ sleep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace sleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.SetSuspendState(PowerState.Suspend, false, true);
+        }
+    }
+}
